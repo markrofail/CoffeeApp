3f5e9fd5cbd9e63affa9595cefc19e1d2dff3ce0
Mark Rofail <markm.rofail@gmail.com>
2023-09-10 19:50:08 +0100

    :sparkles: Add custom fonts

--- App.tsx
+++ App.tsx
@@ -2,11 +2,32 @@ import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { PaperProvider } from "react-native-paper";
+import {
+    useFonts,
+    Roboto_400Regular,
+    Roboto_100Thin,
+    Roboto_300Light,
+    Roboto_500Medium,
+    Roboto_700Bold,
+    Roboto_900Black,
+} from "@expo-google-fonts/roboto";
 import { ROUTES, RootStackParamList } from "./src/routes";
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
 export default function App() {
+    const [fontsLoaded] = useFonts({
+        Roboto_100Thin,
+        Roboto_300Light,
+        Roboto_400Regular,
+        Roboto_500Medium,
+        Roboto_700Bold,
+        Roboto_900Black,
+    });
+
+    if (!fontsLoaded) {
+        return null;
+    }
     return (
         <PaperProvider>
             <NavigationContainer>
